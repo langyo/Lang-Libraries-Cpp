3aba9c277dd4b386aecb4f172a2981c5a66ca598
langyo <m13776491897@163.com>
2017-12-28 20:10:35 +0800

    .

---
+++ NumberSystemCompressAlgorithm/test.cpp
@@ -0,0 +1,17 @@
+#include "algorithm"
+using namespace ly;
+
+int main(){
+	cout<<"输入部分长度为256"<<endl;
+	compresser c;
+	c.p.push_back(random_data());
+	try{
+		c.encompress();
+		cout<<"输出部分长度为"<<c.p.front().size<<endl;
+		cout<<"输出部分循环次数为"<<c.p.front().loop<<endl;
+	}catch(std::runtime_error e){
+		cout<<"[错误]"<<e.what();
+		return 1;
+	}
+	return 0;
+}
