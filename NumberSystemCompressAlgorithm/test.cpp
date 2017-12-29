c73101d7a230bb704e63b91f860dcc8b92bcb2eb
root <root@localhost>
2017-12-29 12:54:49 +0800

    .

--- NumberSystemCompressAlgorithm/test.cpp
+++ NumberSystemCompressAlgorithm/test.cpp
@@ -1,10 +1,11 @@
 #include "algorithm.h"
 using namespace ly;
-
+using namespace std;
 int main(){
 	cout<<"输入部分长度为256"<<endl;
 	compresser c;
-	c.p.push_back(random_data());
+	c.p.emplace_back();
+	c.p.front().data=random_data();
 	try{
 		c.encompress();
 		cout<<"输出部分长度为"<<c.p.front().size<<endl;
