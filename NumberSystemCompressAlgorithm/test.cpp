08b5a92d52524036fd67a2fef909ed61216fdaae
root <root@localhost>
2018-01-01 00:26:13 +0800

    .

--- NumberSystemCompressAlgorithm/test.cpp
+++ NumberSystemCompressAlgorithm/test.cpp
@@ -5,11 +5,11 @@ int main(){
 	cout<<"输入部分长度为256"<<endl;
 	compresser c;
 	c.p.emplace_back();
-	for(auto &i:c.p.front().data) i=0xFF;
+	for(auto &i:c.p.front().data) i=0x6F;
 	try{
 		c.encompress();
 		cout<<"输出部分长度为"<<c.p.front().size<<endl;
-		cout<<"输出部分循环次数为"<<c.p.front().loop<<endl;
+		cout<<"输出部分循环次数为"<<static_cast<int>(c.p.front().loop)<<endl;
 	}catch(std::runtime_error e){
 		cout<<"[错误]"<<e.what();
 		return 1;
